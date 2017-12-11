571c12e077f7953bae2efb4f4e87d7533dea8f8c
Roma Desai <romadesai@Romas-MBP.wv.cc.cmu.edu>
2017-12-10 20:07:23 -0500

    This is for Question 20 of the Final Project.

---
+++ hello_aliens.R
@@ -0,0 +1,20 @@
+
+cat("Hello Friends!
+  
+  Hope you are doing well. My name is Roma and I live on Earth.
+  I would love to know a little more about you and tell you about my people as well. 
+  What do you call yourselves? We can ourselves humans or homo sapiens. Everyone is unique
+  and different. People come from different backgrounds, races, religions, and cultures.
+  What is your planet called? Like I said, our planet is called Earth.
+  Do you enjoy living there? I enjoy living on Earth, but I think it would be cool to visit Mars.
+  Have you ever been to Mars? (Or are you from Mars?) I think there's a lot of cool
+  places to visit on Earth, but haven't seen much of the world. Maybe one day I will be able
+  to travel all across Earth. How did you find this letter? Hope you found it easily.
+  What type of food do you eat? I love to eat pizza and bagels.
+
+  Ok, that's enough for now. Hope you learned something about me and my planet.
+  Would love to hear back from you (if I'm still alive).
+  You can find me at Carnegie Mellon University in Pittsburgh, PA 
+  or call me at (973) 862-1809. 
+    
+  Safe travels.")
